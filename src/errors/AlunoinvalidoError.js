7d94b6ca0342fe440a1c704fe0e8b4d17910bc03
Pedro Neves <henriquepedro52919@gmail.com>
2026-09-25 12:52:25 -0300

    Delete src directory

--- src/errors/AlunoinvalidoError.js
+++
@@ -1,9 +0,0 @@
-const ApiError = require("./ApiError");
-
-class AlunoInvalidoError extends ApiError{
-    constructor(message="nome e email são obg",statusCode = 400){
-        super(message,statusCode);
-    }
-}
-
-module.exports = AlunoInvalidoError;
